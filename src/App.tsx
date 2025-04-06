f78d8cee7ea9435e197091f68c991f1d1bee3ccf
Francisco Esteban <39806686+Dfems@users.noreply.github.com>
2025-04-06 21:26:50 +0000

    fix: test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { useAuth } from './context/AuthContext'
 import BirthdayCountdown from './components/BirthdayCountdown'
